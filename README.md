bdc8465e9df7d29984d3d5f9a85fca06096d419a
Robert Hutzel <amoebob@fastmail.fm>
2017-08-04 20:47:19 -0400

    Documentation update.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Bash script that saves a copy of each JPEG in the working directory to a ./medium/ subdirectory.
-Files that exceed 1.2MB are resized in half.
+Files that exceed 1MB are resized in half. Files that exceed 2MB are resized to a third.
 The convert command comes from the imagemagick binaries.
--- resize-large-pictures.sh
+++ resize-large-pictures.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Saves a copy of each JPEG in the working directory to a "medium" subdirectory.
+# Saves a resized copy of each JPEG in the working directory to a "medium" subdirectory.
 # Files that exceed 1MB are resized in half.
+# Files that exceed 2MB are resized to a third.
 # The convert command comes from the imagemagick binaries.
 
 mkdir --parents medium;
